c3e43894b936fe079bc73f0fd610abdb70084d36
unknown <hann.jang@labinfo.is.escuelaing.edu.co>
2024-10-31 15:11:49 -0500

    Avance

---
+++ src/main/java/org/example/ProxyController.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class ProxyController {
+
+    private String[] services = {
+            "http://ec2-3-91-91-249.compute-1.amazonaws.com:8080/search",
+            "http://ec2-54-87-194-193.compute-1.amazonaws.com:8080/search"
+    };
+    private int currentServiceIndex = 0;
+
+    @GetMapping("/proxy/linearsearch")
+    public ResponseEntity<String> proxyLinearSearch(@RequestParam String list, @RequestParam String value) {
+        String url = getNextServiceUrl() + "/linearsearch?list" + list + "value" + value;
+        return new RestTemplate().getForEntity(url, String.class);
+    }
+
+    @GetMapping("/proxy/binarysearch")
+    public ResponseEntity<String> proxyBinarySearch(@RequestParam String list, @RequestParam String value) {
+        String url = getNextServiceUrl() + "/binarysearch?list" + list + "value"+ value;
+        return new RestTemplate().getForEntity(url, String.class);
+    }
+
+    private String getNextServiceUrl() {
+        String serviceUrl = services[currentServiceIndex];
+        currentServiceIndex = (currentServiceIndex +1);
+        return serviceUrl;
+    }
+}
---
+++ src/main/java/org/example/SearchController.java
@@ -0,0 +1,68 @@
+package org.example;
+
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/search")
+public class SearchController {
+
+    @GetMapping("/linearsearch")
+    public ResponseEntity<Map<String, String>> linearSearch(
+            @RequestParam String list, @RequestParam String value){
+        String[] items = list.split(",");
+        int index = linearSearch(items, value);
+        Map<String, String> response = new HashMap<>();
+        response.put("operation", "linearSearch");
+        response.put("inputlist", list);
+        response.put("value", value);
+        response.put("output", String.valueOf(index));
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/binarysearh")
+    public ResponseEntity<Map<String, String>> binarySearch(
+            @RequestParam String list, @RequestParam String value){
+        String[] items = list.split(",");
+        Arrays.sort(items);
+        int index = binarySearch(items, value, 0, items.length - 1);
+        Map<String, String> response = new HashMap<>();
+        response.put("operation", "binarySearch");
+        response.put("inputlist", list);
+        response.put("value", value);
+        response.put("output", String.valueOf(index));
+        return ResponseEntity.ok(response);
+    }
+
+    private int linearSearch(String[] list, String value){
+        for (int i= 0; i < list.length; i++) {
+            if (list[i].equals(value)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private int binarySearch (String[] list, String value, int left, int right){
+        if (left > right) return -1;
+        int mid = left + (right-left) /2;
+        if (list[mid].equals(value)) return mid;
+        if (list[mid].compareTo(value)>0){
+            return binarySearch(list, value, left, mid -1);
+        } else {
+            return binarySearch(list, value, mid +1, right);
+        }
+
+    }
+
+}
